a113857c584eb6a8231b3f1443862793c0cb1161
Marc C <Marc@d8.uk>
2019-07-08 20:37:02 +0100

    current scrope added for the db connection, is this the best method thought?

--- src/Our.Umbraco.AuthU/Data/UmbracoDbOAuthClientStore.cs
+++ src/Our.Umbraco.AuthU/Data/UmbracoDbOAuthClientStore.cs
@@ -1,21 +1,14 @@
 ﻿using Our.Umbraco.AuthU.Interfaces;
 using Our.Umbraco.AuthU.Models;
-using Umbraco.Core.Scoping;
+using Umbraco.Core.Composing;
 
 namespace Our.Umbraco.AuthU.Data
 {
     public class UmbracoDbOAuthClientStore : IOAuthClientStore
     {
-        private readonly IScopeProvider _scopeProvider;
-
-        public UmbracoDbOAuthClientStore(IScopeProvider scopeProvider)
-        {
-            _scopeProvider = scopeProvider;
-        }
-
         public OAuthClient FindClient(string clientId)
         {
-            using (var scope = _scopeProvider.CreateScope(autoComplete: true))
+            using (var scope = Current.ScopeProvider.CreateScope(autoComplete: true))
             {
                 return scope.Database.SingleOrDefault<OAuthClient>("SELECT * FROM [OAuthClient] WHERE [ClientId] = @0", clientId);
             }
--- src/Our.Umbraco.AuthU/Data/UmbracoDbOAuthRefreshTokenStore.cs
+++ src/Our.Umbraco.AuthU/Data/UmbracoDbOAuthRefreshTokenStore.cs
@@ -1,6 +1,6 @@
 ﻿using Our.Umbraco.AuthU.Interfaces;
 using Our.Umbraco.AuthU.Models;
-using Umbraco.Core.Scoping;
+using Umbraco.Core.Composing;
 
 namespace Our.Umbraco.AuthU.Data
 {
@@ -9,16 +9,9 @@ namespace Our.Umbraco.AuthU.Data
         internal const string CurrentVersion = "1.0.1";
         internal const string SubProductName = "AuthU_UmbracoDbOAuthRefreshTokenStore";
 
-        private readonly IScopeProvider _scopeProvider;
-
-        public UmbracoDbOAuthRefreshTokenStore(IScopeProvider scopeProvider)
-        {
-            _scopeProvider = scopeProvider;
-        }
-
         public void AddRefreshToken(OAuthRefreshToken token)
         {
-            using (var scope = _scopeProvider.CreateScope(autoComplete: true))
+            using (var scope = Current.ScopeProvider.CreateScope(autoComplete: true))
             {
                 scope.Database.Execute("DELETE FROM [OAuthRefreshToken] WHERE [Subject] = @0 AND [UserType] = @1 AND [Realm] = @2 AND [ClientId] = @3 AND [DeviceId] = @4",
                 token.Subject,
@@ -33,7 +26,7 @@ namespace Our.Umbraco.AuthU.Data
 
         public void RemoveRefreshToken(string refreshTokenId)
         {
-            using (var scope = _scopeProvider.CreateScope(autoComplete: true))
+            using (var scope = Current.ScopeProvider.CreateScope(autoComplete: true))
             {
                 scope.Database.Execute("DELETE FROM [OAuthRefreshToken] WHERE [Key] = @0", refreshTokenId);
             }
@@ -41,7 +34,7 @@ namespace Our.Umbraco.AuthU.Data
 
         public OAuthRefreshToken FindRefreshToken(string refreshTokenId)
         {
-            using (var scope = _scopeProvider.CreateScope(autoComplete: true))
+            using (var scope = Current.ScopeProvider.CreateScope(autoComplete: true))
             {
                 return scope.Database.SingleOrDefault<OAuthRefreshToken>("SELECT * FROM [OAuthRefreshToken] WHERE [Key] = @0", refreshTokenId);
             }
